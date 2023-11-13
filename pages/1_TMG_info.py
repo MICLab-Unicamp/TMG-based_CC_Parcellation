ed8d15de777c913cd2cdbd47aa09e6bd193f53c1
Caio PS <caiosantana_06@outlook.com>
2023-11-13 02:44:10 -0300

    General additions to the text

--- pages/1_TMG_info.py
+++ pages/1_TMG_info.py
@@ -2,17 +2,16 @@ import streamlit as st
 
 st.title('Tensorial Morphological Gradient (TMG)')
 
-st.write('The TMG uses the dissimilarity between neighboring voxels to summarize the main tensorial information into a scalar map. It was proposed to facilitate segmentation\
-          and parcellation tasks using DTI data.')
+st.write('The TMG [[1]](#references) uses the dissimilarity between neighboring voxels to summarize the main tensorial information into a scalar map. It adapts the idea of a morphological gradient to tensorial images and\
+          was proposed to facilitate segmentation and parcellation tasks using Diffusion Tensor Imaging (DTI) data.')
 
-st.write('For each voxel $v$ in a tensorial image $f$, the dissimilarity measure $(d_n)$ between any pair of tensors $(T_i,T_j)$ in its neighborhood (defined by the structuring\
-          element $B$) is calculated. The computed gradient will be the maximum dissimilarity among all pairwise dissimilarities:')
+st.write('For each voxel $v$ in a tensorial image $f$, the dissimilarity measure $(d_n)$ between any pair of tensors $(T_i,T_j)$ in its neighborhood is calculated. The neighborhood is defined\
+          by a structuring element $B$. The computed gradient will be the maximum dissimilarity among all pairwise dissimilarities:')
 
 st.latex(r'''\nabla_B^T(f)(v) = \bigvee_{i,j \in B_v} d_n(T_i, T_j)''')
 
-st.write('There are several dissimilarity measures proposed for DTI applications and explored through the TMG (...)')
-
-st.write('The choice of the structuring element generally depends on the information required (bi- or three-dimensional) and the size of the studied structure (...)')
+st.write('There are several dissimilarity measures proposed for DTI applications and explored through the TMG [[1]](#references). The choice of the structuring element generally depends on the information\
+          required (bi- or three-dimensional) and the size of the studied structure - smaller structuring elements tend to generate thinner, more detailed borders.')
 
 #---------------------------------------
 
@@ -22,10 +21,10 @@ st.header('Dissimilarity measures')
 
 st.subheader('Dot Product (prod)')
 
-st.write('Only takes in consideration the main eigenvectors. The result varies between 0 and 1 according to the angle between the vectors: equals to one if the vectors are\
+st.write('The Dot Product [[2]](#references) only takes in consideration the main eigenvectors. The result varies between 0 and 1 according to the angle between the vectors: equals to one if the vectors are\
           parallel and equals to zero if the vectors are orthogonal.')
 
-st.latex(r'''d_{DP}(T_i, T_j) = |e_{1i} \cdot e_{1j}|''')
+st.latex(r'''d_{prod}(T_i, T_j) = |e_{1i} \cdot e_{1j}|''')
 
 st.write('Since the Dot Product is a similarity measure, the TMG calculates the minimal similarity instead of the maximal dissimilarity. The negative of the resulting image\
           will be the output of the TMG (maintaining the same pattern as for the other measures).')
@@ -34,21 +33,30 @@ st.write('Since the Dot Product is a similarity measure, the TMG calculates the 
 
 st.subheader('Frobenius Norm (frob)')
 
+st.write('The Frobenius Norm [[3]](#references) is an Euclidean distance measure between the tensors, as follows:')
 
+st.latex(r'''d_{frob}(T_i,T_j) = \sqrt{trace((T_i - T_j)^2)}''')
+
+st.write('This dissimilarity measure is not invariant to affine transformations of rotation. However, it presents a linear response to differences in anisotropy and trace.')
 
 #-----------------
 
 st.subheader('J-Divergence (Jdiv)')
 
+st.write('The J-divergence [[4]](#references) is a tensorial "distance" measure, based on concepts of theory information. The measure is not truly a distance, since it violates the triangular inequality. However, \
+         it is a computationally efficient approximation of the Rao distance and is invariant to affine transformations.')
 
+st.latex(r'''d_{Jdiv}(T_i,T_j) = \frac{1}{2} \sqrt{trace(T_i^{-1}T_j + T_j^{-1}T_i) - 2n}''')
+
+st.markdown("""with $n$ being the size of the square matrix $T$.""")
 
 #-----------------
 
 st.subheader('Log-Euclidean distance (logE)')
 
-st.write('The LogE is basically an Euclidean distance measure between the logarithm of the tensors, as follows:')
+st.write('The logE [[5]](#references) is an Euclidean distance measure between the logarithm of the tensors, close related to the Frobenius Norm.')
 
-st.latex(r'''d_{LE}(T_i, T_j) = \sqrt{trace((\log(T_i)-\log(T_j))^2)}''')
+st.latex(r'''d_{logE}(T_i, T_j) = \sqrt{trace((\log(T_i)-\log(T_j))^2)}''')
 
 st.write('From the equation above, it is possible to see that symmetric matrices with null or negative eigenvalues are at an infinite distance from any tensor.\
           To overcome this problem, $\log(T_i)$ was replaced with $E_i \cdot \log(100\lambda_i+1)I\cdot E_i^T$, where $E$ corresponds to the matrix of eigenvectors,\
@@ -61,3 +69,19 @@ st.header('Structuring elements')
 #...
 
 st.subheader('Visualization')
+
+#---------------------------------------
+
+st.subheader('References')
+
+st.markdown("""[1] Rittner, L., Campbell, J. S., Freitas, P. F., Appenzeller, S., Bruce Pike, G., Lotufo, R. A. (2013). Analysis of scalar maps for the segmentation of the corpus callosum \
+            in diffusion tensor fields. _Journal of mathematical imaging and vision_, doi: [10.1007/s10851-012-0377-4](https://doi.org/10.1007/s10851-012-0377-4).""")
+
+st.markdown("""[2] Ziyan, U., Tuch, D., Westin, C. F. (2006). Segmentation of thalamic nuclei from DTI using spectral clustering. In _Medical Image Computing and Computer-Assisted Intervention–MICCAI 2006_, doi: \
+            [10.1007/11866763_99](https://doi.org/10.1007/11866763_99).""")
+
+st.markdown("""[3]""")
+
+st.markdown("""[4]""")
+
+st.markdown("""[5]""")
